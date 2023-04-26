63b3023f3ecac9893ad156ad8afc162e75b18ad4
leonardo3909 <127439867+leonardo3909@users.noreply.github.com>
2023-04-25 22:49:15 -0500

    Add files via upload
    Entrega_los_10_metodos_propuestos_en_clase

---
+++ tads-master/tads-master/src/main/java/co/edu/umanizales/tads/controller/dto/KidDTO.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.tads.controller.dto;
+
+import co.edu.umanizales.tads.model.GenderKid;
+import lombok.Data;
+
+@Data
+public class KidDTO {
+    private String identification;
+    private String name;
+    private byte age;
+    private char gender;
+    private String codeLocation;
+}
---
+++ tads-master/tads-master/src/main/java/co/edu/umanizales/tads/model/GenderKid.java
@@ -0,0 +1,9 @@
+package co.edu.umanizales.tads.model;
+
+import lombok.Data;
+
+@Data
+public class GenderKid {
+    private String code;
+    private char gender;
+}
